9b2c06d0f5c3279c78aa9149ee2297251a668c5f
Vishwas Ramesh <cuttingedge6034@gmail.com>
2021-05-17 12:27:31 +0530

    Add GET, PUT, POST and DELETE

---
+++ src/main/java/com/vishwas/messenger/database/DatabaseClass.java
@@ -0,0 +1,20 @@
+package com.vishwas.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.vishwas.messenger.model.*;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+	
+	public static Map<Long, Message> getMessages(){
+		return messages;
+	}
+	
+	public static Map<Long, Profile> getProfiles(){
+		return profiles;
+	}
+}
--- src/main/java/com/vishwas/messenger/service/MessageService.java
+++ src/main/java/com/vishwas/messenger/service/MessageService.java
@@ -1,19 +1,45 @@
 package com.vishwas.messenger.service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 
+import com.vishwas.messenger.database.DatabaseClass;
 import com.vishwas.messenger.model.Message;;
 
 public class MessageService {
 	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1L, "Hello Vishwas", "Vishwas"));
+		messages.put(2L, new Message(2L, "Hello Aishwarya", "Aishwarya"));
+	}
+	
 	public List<Message> getAllMessages(){
-		Message m1 = new Message(1L, "Hello Vishwas", "Vishwas");
-		Message m2 = new Message(1L, "Hello","Aish");
-		List<Message> list= new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;		
+		return new ArrayList<Message>(messages.values());		
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
 	}
 
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if(message.getId()<=0) {
+			return null;
+		}
+		
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		 return messages.remove(id);
+	}
 }
