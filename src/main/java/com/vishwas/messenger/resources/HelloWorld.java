bb2b852a400b5591f6696af677736be866aea9cc
Vishwas Ramesh <cuttingedge6034@gmail.com>
2021-05-19 12:34:00 +0530

    Integrate with swagger core to generate {api_path}/swagger.json

--- src/main/java/com/vishwas/messenger/resources/HelloWorld.java
+++ src/main/java/com/vishwas/messenger/resources/HelloWorld.java
@@ -5,8 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 @Path("/hello")
+@Api(value="/hello", description="Returns Hello world in plain text")
 public class HelloWorld {
+	@ApiOperation(value="Print Hello world!")
+	@ApiResponses(value = { 
+			@ApiResponse(code = 400, message =  "Invalid department id supplied"),
+			@ApiResponse(code = 404, message = "Department not found")
+			})
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
